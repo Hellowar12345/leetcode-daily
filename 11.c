20610150e2f4a507e30b921d13b3d2158a1f6995
李欣員 <144247183+Hellowar12345@users.noreply.github.com>
2025-10-02 20:43:54 +0800

    Create 11.c

---
+++ 11.c
@@ -0,0 +1,20 @@
+int min(int a,int b){
+    if(a>b)return b;
+    else return a;
+}
+int maxArea(int* height, int heightSize) {
+    int left=0;
+    int right=heightSize-1;
+    int high=min(height[left],height[right]);
+    int max=(right-left)*high;
+    if(high==height[left])left++;
+    else right--;
+    while(left!=right){
+        high=min(height[left],height[right]);
+        int tmp=(right-left)*high;
+        if(tmp>max)max=tmp;
+        if(high==height[left])left++;
+        else right--;
+    }
+    return max;
+}
